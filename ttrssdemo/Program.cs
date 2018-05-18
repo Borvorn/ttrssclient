78f04e2eb061d0db71cf9accf2759569667b4b8e
Borvorn <beyond.my.limit@gmail.com>
2018-05-18 15:52:34 +0700

    modified demo

--- ttrssdemo/Program.cs
+++ ttrssdemo/Program.cs
@@ -18,47 +18,54 @@
         {
             /*System.Net.HttpWebRequest adds the header 'HTTP header "Expect: 100-Continue"' to every request unless you explicitly ask it not to by setting this static property to false:*/
             System.Net.ServicePointManager.Expect100Continue = false;
-
-            var server = ConfigurationManager.AppSettings["server"];
-            var username = ConfigurationManager.AppSettings["username"];
+            /*
+            var server = ConfigurationManager.AppSettings["http://192.168.100.9/tt-rss/api/"];
+            var username = ConfigurationManager.AppSettings["user"];
             var password = ConfigurationManager.AppSettings["password"];
+            */
 
-            var response = webClient.UploadString(server, serializer.Serialize(GetLoginRequest(username, password)));
-            var loginResponse = serializer.Deserialize<Response<LoginResponse>>(response);
+            string server = "http://example.com/tt-rss/api/";
+            string username = "user";
+            string password = "password";
 
-            var session_id = loginResponse.content.session_id;
+            /*
+            Console.Write("Url : ");
+            string server = Console.ReadLine();
+            Console.Write("User : ");
+            string username = Console.ReadLine();
+            Console.Write("Password : ");
+            string password = Console.ReadLine();
+            */
+
+            string response = webClient.UploadString(server, serializer.Serialize(GetLoginRequest(username, password)));
+            Response<LoginResponse> loginResponse = serializer.Deserialize<Response<LoginResponse>>(response);
+
+            string session_id = loginResponse.content.session_id;
 
             Console.WriteLine("successfully logged in, session_id: " + session_id);
 
-            var getUnreadReqeust = new GetUnreadRequest(session_id);
-            var unread = ExecuteMethod<GetUnreadRequest, GetUnreadResponse>(getUnreadReqeust, server).unread;
+            GetUnreadRequest getUnreadReqeust = new GetUnreadRequest(session_id);
+            int unread = ExecuteMethod<GetUnreadRequest, GetUnreadResponse>(getUnreadReqeust, server).unread;
             Console.WriteLine("unread number of articles: " + unread);
 
-            var getCountersRequest = new GetCountersRequest(session_id, "l");
-            var counters = ExecuteMethod<GetCountersRequest, Counter[]>(getCountersRequest, server);
+            GetCountersRequest getCountersRequest = new GetCountersRequest(session_id, "l");
+            Counter[] counters = ExecuteMethod<GetCountersRequest, Counter[]>(getCountersRequest, server);
             Console.WriteLine(counters);
 
             Console.WriteLine("Categories: ");
-            var categories = ExecuteMethod<GetCategoriesRequest, Category[]>(new GetCategoriesRequest(session_id, true, false, false), server);
-            foreach (var categroy in categories)
+            Category[] categories = ExecuteMethod<GetCategoriesRequest, Category[]>(new GetCategoriesRequest(session_id, true, false, false), server);
+            foreach (Category categroy in categories)
             {
                 Console.WriteLine(categroy.title + ": " + categroy.unread);
             }
             Console.WriteLine();
 
-            Console.WriteLine("Feeds: ");
-            var feeds = ExecuteMethod<GetFeedsRequest, Feed[]>(new GetFeedsRequest(session_id, -3, true, true), server);
-            foreach (var feed in feeds)
-            {
-                Console.WriteLine(feed.title + ": " + feed.unread);
-            }
+            Showfeed(session_id, server);
+
             Console.WriteLine();
 
-            Console.WriteLine("HeadLines of Feed 34");
-            var headLines = ExecuteMethod<GetHeadLinesRequest, HeadLine[]>(new GetHeadLinesRequest(session_id, 34), server);
-
-            var logoutObject = new LogoutRequest(session_id);
-            var logoutResponse = ExecuteMethod<LogoutRequest, LogoutResponse>(logoutObject, server);
+            LogoutRequest logoutObject = new LogoutRequest(session_id);
+            LogoutResponse logoutResponse = ExecuteMethod<LogoutRequest, LogoutResponse>(logoutObject, server);
             Console.WriteLine(logoutResponse.status);
 
             Console.ReadKey();
@@ -77,8 +84,27 @@
 
         static U ExecuteMethod<T, U>(T Request, string url)
         {
-            var response = webClient.UploadString(url, serializer.Serialize(Request));
+            string response = webClient.UploadString(url, serializer.Serialize(Request));
             return serializer.Deserialize<Response<U>>(response).content;
+        }
+
+        static void Showfeed(string session_id, string server)
+        {
+            Console.WriteLine("Feeds: ");
+            Feed[] feeds = ExecuteMethod<GetFeedsRequest, Feed[]>(new GetFeedsRequest(session_id, -3, true, true), server);
+            foreach (Feed feed in feeds)
+            {
+                Console.WriteLine(feed.title + ": " + feed.unread);
+
+                Console.WriteLine("HeadLines of Feed" + " " + feed.id);
+                HeadLine[] headLines = ExecuteMethod<GetHeadLinesRequest, HeadLine[]>(new GetHeadLinesRequest(session_id, Int32.Parse(feed.id)), server);
+                foreach (HeadLine head in headLines)
+                {
+                    Console.WriteLine(head.id + " " + head.title);
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
         }
     }
 }
